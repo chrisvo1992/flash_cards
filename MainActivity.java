4d219ebf07b6c2c41fa3957518f555bb5607df98
chrisvo1992 <59588811+chrisvo1992@users.noreply.github.com>
2022-03-03 20:15:55 -0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,3 +1,10 @@
+package com.example.cvoflashcardsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -7,9 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ID's for flashcards elements
         TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
         TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
+        TextView multipleChoice1 = findViewById(R.id.answer_box_1_textview);
+        TextView multipleChoice2 = findViewById(R.id.answer_box_2_textview);
+        TextView multipleChoice3 = findViewById(R.id.answer_box_3_textview);
 
+        //OnClick listener for flashcard question
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -19,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        //OnClick listener for flashcard answer
         flashcardAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +41,35 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        //OnClick listener for multiple choice answer (correct answer)
+        multipleChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                multipleChoice1.setBackgroundColor(getResources().getColor(R.color.green, null));
+
+
+            }
+
+        });
+        //OnClick listener for multiple choice answer (wrong answer)
+        multipleChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                multipleChoice1.setBackgroundColor(getResources().getColor(R.color.green, null));
+                multipleChoice2.setBackgroundColor(getResources().getColor(R.color.red, null));
+
+
+            }
+
+        });
+        //OnClick listener for multiple choice answer (wrong answer)
+        multipleChoice3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                multipleChoice1.setBackgroundColor(getResources().getColor(R.color.green, null));
+                multipleChoice3.setBackgroundColor(getResources().getColor(R.color.red, null));
+
+            }
+
+        });
     }
-}
