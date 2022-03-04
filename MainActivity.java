619c716e850b6d4b940fe30020adf5208831e037
chrisvo1992 <59588811+chrisvo1992@users.noreply.github.com>
2022-03-03 18:54:03 -0800

    Lab 2 java file

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
+
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+
+            }
+
+        });
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+
+            }
+
+        });
+    }
+}
